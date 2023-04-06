968a8d508a54b5fa88a92a19ab44d536825011d0
21BCA02 <96609308+21BCA02@users.noreply.github.com>
2023-04-06 18:23:04 +0530

    Add files via upload

---
+++ solution/solution5.java
@@ -0,0 +1,25 @@
+/*Write a program that will accept a number from command line and raise a user defined
+exception if the number consists of odd number of digits.*/
+class OddException extends Exception{
+    OddException(String str){
+        System.out.println(str);
+    }
+}
+class ExceptionHandaling{
+    public static void main(String args[]){
+        int a=Integer.parseInt(args[0]);
+        try{
+            if(a%2==0){
+                System.out.println("The "+a+" is Even.");
+            }
+            else{
+                throw new OddException("Number Consist Odd Value.");
+            }
+
+        }
+        catch(OddException e){
+            System.out.println(e);;
+        }
+
+    }
+}
